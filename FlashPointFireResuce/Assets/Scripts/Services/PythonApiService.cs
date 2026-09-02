65c38184db9a5935c2b4a96672a60ca66fedcd12
Citybanamex16 <carlosdc2005@icloud.com>
2026-09-01 18:41:23 -0600

    Avance Conexion Unity & Python

--- FlashPointFireResuce/Assets/Scripts/Services/PythonApiService.cs
+++ FlashPointFireResuce/Assets/Scripts/Services/PythonApiService.cs
@@ -7,27 +7,19 @@ public class PythonApiService
 {
     private const string URL = "http://127.0.0.1:5000/api/process";
 
-    public async Task<PythonResponse> SendDataToPythonAsync(PlayerDataPayload payload)
-    {
-        // 1. Convertimos el objeto C# a String JSON
-        string jsonString = JsonUtility.ToJson(payload);
-        byte[] bodyRaw = Encoding.UTF8.GetBytes(jsonString);
+      public async Task<SetupDTO> requestSetupDTO(){
 
-        // 2. Preparamos la petición HTTP POST
-        using (UnityWebRequest request = new UnityWebRequest(URL, "POST"))
+        // 1. Preparamos la petición HTTP POST
+        using (UnityWebRequest request = new UnityWebRequest(URL, "GET"))
         {
-            request.uploadHandler = new UploadHandlerRaw(bodyRaw);
-            request.downloadHandler = new DownloadHandlerBuffer();
-            request.SetRequestHeader("Content-Type", "application/json");
-
-            // 3. Enviamos y esperamos asíncronamente (sin congelar el juego a 60 FPS)
+            // 2. Enviamos y esperamos asíncronamente (sin congelar el juego a 60 FPS)
             var operation = request.SendWebRequest();
             while (!operation.isDone)
             {
                 await Task.Yield(); // Cede el control en cada frame hasta completar
             }
 
-            // 4. Verificamos errores de red
+            // 3. Verificamos errores de red
             if (request.result != UnityWebRequest.Result.Success)
             {
                 Debug.LogError($"[API Error]: {request.error}");
@@ -36,7 +28,7 @@ public class PythonApiService
 
             // 5. Convertimos el JSON de respuesta devuelto por Python en nuestro Modelo C#
             string jsonResponse = request.downloadHandler.text;
-            return JsonUtility.FromJson<PythonResponse>(jsonResponse);
+            return JsonUtility.FromJson<SetupDTO>(jsonResponse);
         }
     }
 }
